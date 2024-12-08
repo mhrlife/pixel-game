60423f2f9e1393f5a2587448a9101dedfd7b9605
Mohammad <=>
2024-12-08 17:31:28 +0330

    Add logo as import

--- ui/src/components/HeaderUserInfo.tsx
+++ ui/src/components/HeaderUserInfo.tsx
@@ -6,6 +6,7 @@ import {fetchOnlineUsersCount} from "../store/stats.ts";
 import {useAppDispatch, useAppSelector} from "../store/store.ts";
 import {useEffect} from "react";
 import {forceFarsiNumbers} from "../utils.ts";
+import logo from "../../public/pixel-logo.jpg";
 
 export function HeaderUserInfo() {
     const dispatch = useAppDispatch();
@@ -28,7 +29,7 @@ export function HeaderUserInfo() {
     return <div className={styles.HeaderUserInfo}>
         <Row align={'center'} justify={'space-between'}>
             <Row align={'center'} justify={'center'}>
-                <img src="/pixel-logo.jpg?v=1" alt="Logo"/>
+                <img src={logo} alt="Logo"/>
                 <Row align={'center'} justify={'center'} direction={'column'} gap={'0'}>
                     <h3>تصدانه</h3>
                     <Paragraph size={'s'} caption={true}>
